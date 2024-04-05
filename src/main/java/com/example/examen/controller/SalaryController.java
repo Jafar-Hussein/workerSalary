b045cc6949d3c5bdb627c2858868a0e4fbfff629
Jafar Hussein <Jafarhussein6@gmail.com>
2024-04-05 17:23:16 +0200

    Fixade fel

--- src/main/java/com/example/examen/controller/SalaryController.java
+++ src/main/java/com/example/examen/controller/SalaryController.java
@@ -14,7 +14,7 @@ import java.time.YearMonth;
 
 @RestController
 @RequestMapping("/salary/")
-@RequiredArgsConstructorg
+@RequiredArgsConstructor
 @CrossOrigin(origins= "*")
 public class SalaryController {
     private final SalaryService salaryService;
