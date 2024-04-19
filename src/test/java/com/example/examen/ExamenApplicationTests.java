d5be394d2e5add78e6bf46f70967b4edae28c80f
Jafar Hussein <Jafarhussein6@gmail.com>
2024-04-19 15:02:17 +0200

    fix

--- src/test/java/com/example/examen/ExamenApplicationTests.java
+++ src/test/java/com/example/examen/ExamenApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.examen;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ExamenApplication.class)
 class ExamenApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
